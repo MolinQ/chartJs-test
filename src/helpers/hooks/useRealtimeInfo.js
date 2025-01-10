b9e1d1cbf6920785150bf8477c7bfb8d9a3b59dd
Kiril krasnozhon <kiril.krasnozhon@cleveroad.com>
2025-01-10 12:48:46 +0200

    remove file

--- src/helpers/hooks/useRealtimeInfo.js
+++ src/helpers/hooks/useRealtimeInfo.js
@@ -1,18 +1,15 @@
 import { useEffect, useState } from "react";
 import useWebSocket from "react-use-websocket";
-import { baseUrl } from "../../constants/baseUrl.js";
 import { CutArray } from "../cutArray.js";
+import { baseUrl } from "../../constants/baseUrl.js";
 
 export const useRealtimeInfo = () => {
   const [crypto, setCrypto] = useState([]);
   const [newCrypto, setNewCrypto] = useState({});
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
-    baseUrl,
-    {
-      share: false,
-      shouldReconnect: () => true,
-    },
-  );
+  const { lastJsonMessage } = useWebSocket(baseUrl, {
+    share: false,
+    shouldReconnect: () => true,
+  });
 
   useEffect(() => {
     const day = new Date(`December 17, 1995 03:24:00`);
